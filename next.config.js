b9a6d82defc5d3267009774667357668271bb779
Simu <zaugg.simon@protonmail.com>
2021-08-26 12:56:45 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+module.exports = {
+  env: {
+    ENDPOINT:
+      "https://api-eu-central-1.graphcms.com/v2/cksri5lo0428q01y2hlstb470/master",
+  },
+};
